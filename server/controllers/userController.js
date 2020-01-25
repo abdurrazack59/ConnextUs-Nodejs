25a54ec0e0e55ff5cc06108d065228d46934bea9
abdurrazack59 <55846628+abdurrazack59@users.noreply.github.com>
2020-01-25 22:45:35 +0530

    25/1/2020

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
-const User = require("../models/user");
+const User = require("../models/userModel");
 const roles = require("../roles");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 async function hashPassword(password) {
   return await bcrypt.hash(password, 10);
@@ -14,31 +14,24 @@ async function validatePassword(plainPassword, hashedPassword) {
 exports.signup = async (req, res, next) => {
   try {
     const { email, password, role } = req.body;
-
     const hashedPassword = await hashPassword(password);
-
     const newUser = new User({
       email,
       password: hashedPassword,
       role: role || "basic"
     });
-
-    const accessToken = jwt.sign(
-      { userId: newUser._id },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1d"
-      }
-    );
-
-    newUser.accessToken = accessToken;
-
+    // const accessToken = jwt.sign(
+    //   { userId: newUser._id },
+    //   process.env.JWT_SECRET,
+    //   {
+    //     expiresIn: "1d"
+    //   }
+    // );
+    // newUser.accessToken = accessToken;
     await newUser.save();
-
     res.json({
       data: newUser,
-
-      accessToken
+      // accessToken
     });
   } catch (error) {
     next(error);
@@ -48,24 +41,20 @@ exports.signup = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
     const user = await User.findOne({ email });
-
-    if (!user) return next(new Error("Email does not exist"));
-
+    if (!user){
+      res.status(401).json({message:"Email does not exist"});
+    } 
     const validPassword = await validatePassword(password, user.password);
-
-    if (!validPassword) return next(new Error("Password is not correct"));
-
+    if (!validPassword) {
+      res.status(422).json({message:"Password is not correct"});
+    } 
     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d"
     });
-
     await User.findByIdAndUpdate(user._id, { accessToken });
-
     res.status(200).json({
       data: { email: user.email, role: user.role },
-
       accessToken
     });
   } catch (error) {
@@ -75,7 +64,6 @@ exports.login = async (req, res, next) => {
 
 exports.getUsers = async (req, res, next) => {
   const users = await User.find({});
-
   res.status(200).json({
     data: users
   });
@@ -84,11 +72,10 @@ exports.getUsers = async (req, res, next) => {
 exports.getUser = async (req, res, next) => {
   try {
     const userId = req.params.userId;
-
     const user = await User.findById(userId);
-
-    if (!user) return next(new Error("User does not exist"));
-
+    if (!user){
+      return next(new Error("User does not exist"));
+    }
     res.status(200).json({
       data: user
     });
@@ -100,16 +87,11 @@ exports.getUser = async (req, res, next) => {
 exports.updateUser = async (req, res, next) => {
   try {
     const update = req.body;
-
     const userId = req.params.userId;
-
     await User.findByIdAndUpdate(userId, update);
-
     const user = await User.findById(userId);
-
     res.status(200).json({
       data: user,
-
       message: "User has been updated"
     });
   } catch (error) {
@@ -149,12 +131,10 @@ exports.grantAccess = function(action, resource) {
 exports.allowIfLoggedin = async (req, res, next) => {
   try {
     const user = res.locals.loggedInUser;
-
     if (!user)
       return res.status(401).json({
         error: "You need to be logged in to access this route"
       });
-
     req.user = user;
     next();
   } catch (error) {
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const path = require("path");
 const routes = require("./routes/route.js");
-const User = require("./models/user");
+const User = require("./models/userModel");
 const cors = require("cors");
 const dbURI = require("./config/db");
 const mongoose = require("mongoose");
@@ -15,10 +15,10 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/api", api);
+app.use("/api", routes);
 
 require("dotenv").config({
-  path: path.join(server, "./.env")
+  path: path.join(__dirname, "./.env")
 });
 
 // connection to database
@@ -41,22 +41,16 @@ mongoose.connect(
 app.use(async (req, res, next) => {
   if (req.headers["x-access-token"]) {
     const accessToken = req.headers["x-access-token"];
-
     const { userId, exp } = await jwt.verify(
       accessToken,
       process.env.JWT_SECRET
     );
-
     // Check if token has expired
-
     if (exp < Date.now().valueOf() / 1000) {
-      return res
-        .status(401)
-        .json({
+      return res.status(401).json({
           error: "JWT token has expired, please login to obtain a new one"
         });
     }
-
     res.locals.loggedInUser = await User.findById(userId);
     next();
   } else {
@@ -64,7 +58,6 @@ app.use(async (req, res, next) => {
   }
 });
 
-app.use("/", routes);
 app.listen(PORT, () => {
   console.log("Server is listening on Port:", PORT);
 });
