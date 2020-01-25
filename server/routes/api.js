063c75eaee85c3d1d11c0a5eb843b37d2478385c
abdurrazack59 <55846628+abdurrazack59@users.noreply.github.com>
2020-01-25 14:26:51 +0530

    25/1/2020

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const jwtSecretKey = require("../config/jwt-secret-key");
 
-
-
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = new User(
+      _.pick(req.body, ["name", "email", "password", "role"])
+    );
+    await user.save();
+    const token = jwt.sign({ _id: user._id, role: user.role }, "secretkey");
+    res
+      .header("x-auth-header", token)
+      .send(_.pick(user, ["name", "email", "password"]));
+  } catch (ex) {
+    res.status(401).send("User unable to get auth token::");
+  }
+});
 
 module.exports = router;
