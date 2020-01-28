49863ae537e20b29b98a0993d11f900e78700ec4
abdurrazack59 <55846628+abdurrazack59@users.noreply.github.com>
2020-01-28 18:59:03 +0530

    28/1/2020

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
 
 require("dotenv").config({
