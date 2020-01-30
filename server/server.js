49cd19027982269aa847ac82d32626317107c20b
abdurrazack59 <55846628+abdurrazack59@users.noreply.github.com>
2020-01-30 18:41:10 +0530

    30/1/2020

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
 const path = require("path");
 const routes = require("./routes/route.js");
-const User = require("./models/userModel");
 const cors = require("cors");
-const dbURI = require("./config/db");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
@@ -24,7 +21,7 @@ require("dotenv").config({
 
 // connection to database
 mongoose.connect(
-  dbURI,
+  process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   error => {
     if (error) {
@@ -35,25 +32,6 @@ mongoose.connect(
   }
 );
 
-app.use(async (req, res, next) => {
-  if (req.headers["x-access-token"]) {
-    const accessToken = req.headers["x-access-token"];
-    const { userId, exp } = await jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET
-    );
-    // Check if token has expired
-    if (exp < Date.now().valueOf() / 1000) {
-      return res.status(401).json({
-          error: "JWT token has expired, please login to obtain a new one"
-        });
-    }
-    res.locals.loggedInUser = await User.findById(userId);
-    next();
-  } else {
-    next();
-  }
-});
 
 app.listen(PORT, () => {
   console.log("Server is listening on Port:", PORT);
